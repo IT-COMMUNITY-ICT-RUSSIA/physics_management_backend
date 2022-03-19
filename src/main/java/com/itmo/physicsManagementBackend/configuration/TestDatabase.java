a5823d3c0722031c9c2174464c97e231c167ed0a
Daza10samu <andreyhigher@gmail.com>
2022-03-19 21:44:59 +0300

    feat: add test profile

---
+++ src/main/java/com/itmo/physicsManagementBackend/configuration/TestDatabase.java
@@ -0,0 +1,33 @@
+package com.itmo.physicsManagementBackend.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+@Configuration
+@Profile("dev")
+public class TestDatabase {
+    @Value("#{systemEnvironment['DB_HOST']}")
+    private String dbHost;
+    @Value("#{systemEnvironment['DB_NAME']}")
+    private String dbName;
+    @Value("#{systemEnvironment['DB_USER_NAME']}")
+    private String userName;
+    @Value("#{systemEnvironment['DB_PASSWORD']}")
+    private String passsword;
+
+    @Bean
+    public DriverManagerDataSource getDataSource() {
+        DriverManagerDataSource bds = new DriverManagerDataSource();
+        bds.setDriverClassName("org.postgresql.Drive");
+        bds.setUrl("jdbc:mysql://%s:5432/%s".formatted(dbHost, dbName));
+        bds.setUsername(userName);
+        bds.setPassword(passsword);
+
+        return bds;
+    }
+
+}

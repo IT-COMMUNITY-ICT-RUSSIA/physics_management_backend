9c01de3b2bb92dbabe7dc6af1c8c1d02e8d88e10
Daza10samu <andreyhigher@gmail.com>
2022-03-19 20:01:50 +0300

    init: init spring boot application

---
+++ src/main/java/com/itmo/physicsManagementBackend/PhysicsManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.physicsManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicsManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicsManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmo/physicsManagementBackend/PhysicsManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.physicsManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicsManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
